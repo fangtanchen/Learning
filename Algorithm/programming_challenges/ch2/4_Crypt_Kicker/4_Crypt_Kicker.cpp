5140a1f43803d30216ed94c505f027cd9e090eb4
fangtanchen <chentf0822_account@126.com>
2015-11-08 10:56:40 +0800

    crypt kicker

---
+++ Algorithm/programming_challenges/ch2/4_Crypt_Kicker/4_Crypt_Kicker.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<vector>
+#include<map>
+
+
+using namespace std;
+
+#define U_DEBUG
+#define ONLINE_JUDGE
+
+vector< vector< char > > dic;
+vector< vector< char > > words;
+map< char, char > letters;
+
+int main(){
+
+#ifdef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		
+	}
+
+#ifdef U_DEBUG
+	fclose(stdin);
+	fclose(stdout);
+#endif
+
+	return 0;
+}
