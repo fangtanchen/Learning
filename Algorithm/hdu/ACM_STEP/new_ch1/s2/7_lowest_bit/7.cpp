b7524090015cdff6b7213a13bb7bd7ed38ef1c3f
fangtanchen <chentf0822_account@126.com>
2016-01-21 17:48:01 +0800

    finish chapter1 section2

--- Algorithm/hdu/ACM_STEP/new_ch1/s2/7_lowest_bit/7.cpp
+++ Algorithm/hdu/ACM_STEP/new_ch1/s2/7_lowest_bit/7.cpp
@@ -3,8 +3,8 @@
 //#include<vector>
 #include<algorithm>
 
-#define U_DEBUG
-#define L_JUDGE
+//#define U_DEBUG
+//#define L_JUDGE
 
 #ifdef L_JUDGE
 #pragma warning(disable:4996)
@@ -17,6 +17,19 @@ int main(){
 		freopen("in.txt","r",stdin);
 		freopen("out.txt","w",stdout);
 	#endif
+	
+	int a=0x0001;
+	int n;
+	while(cin>>n,n){
+		a=0x0001;
+		while((n&a)==0){
+			a=a<<1;
+			#ifdef U_DEBUG
+				cout<<a<<endl;
+			#endif
+		}
+		cout<<a<<endl;
+	}
 
 	#ifdef L_JUDGE
 		fclose(stdin);
--- Algorithm/hdu/ACM_STEP/new_ch1/s2/8_ascii/8.cpp
+++ Algorithm/hdu/ACM_STEP/new_ch1/s2/8_ascii/8.cpp
@@ -3,8 +3,8 @@
 //#include<vector>
 #include<algorithm>
 
-#define U_DEBUG
-#define L_JUDGE
+//#define U_DEBUG
+//#define L_JUDGE
 
 #ifdef L_JUDGE
 #pragma warning(disable:4996)
@@ -17,7 +17,18 @@ int main(){
 		freopen("in.txt","r",stdin);
 		freopen("out.txt","w",stdout);
 	#endif
-
+	
+	int T;
+	cin>>T;
+	int num=0;
+	char s[1010];
+	for(int i=0;i<T;i++){
+		cin>>num;
+		s[i]=num;
+	}
+	s[T]=0;
+	cout<<s;
+	
 	#ifdef L_JUDGE
 		fclose(stdin);
 		fclose(stdout);
