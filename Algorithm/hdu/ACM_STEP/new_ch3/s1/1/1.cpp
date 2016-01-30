5bf51bbade1765295fbf4d1317ebccdd070356a6
fangtanchen <chentf0822_account@126.com>
2016-01-30 22:59:02 +0800

    chaojilouti

---
+++ Algorithm/hdu/ACM_STEP/new_ch3/s1/1/1.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+//#include<vector>
+#include<algorithm>
+
+//#define U_DEBUG
+//#define L_JUDGE
+
+#ifdef L_JUDGE
+#pragma warning(disable:4996)
+#endif
+
+using namespace std;
+
+int main(){
+	#ifdef L_JUDGE
+		freopen("in.txt","r",stdin);
+		freopen("out.txt","w",stdout);
+	#endif
+	
+	int f[50];
+		f[1]=0;
+		f[2]=1;
+		f[3]=2;
+		for(int i=4;i<=40;i++){
+			f[i]=f[i-1]+f[i-2];
+		}
+		
+	int N;
+	cin>>N;
+	while(N--){
+		int m;
+		cin>>m;
+		printf("%d\n",f[m]);
+		
+	}
+	#ifdef L_JUDGE
+		fclose(stdin);
+		fclose(stdout);
+		system("out.txt");
+	#endif
+
+	return 0;
+}
