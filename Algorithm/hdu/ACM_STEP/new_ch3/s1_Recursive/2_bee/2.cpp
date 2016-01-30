31c94ab10d366500e81f35955d1545d27955d142
fangtanchen <chentf0822_account@126.com>
2016-01-31 00:17:00 +0800

    recursive problems

--- Algorithm/hdu/ACM_STEP/new_ch3/s1_Recursive/2_bee/2.cpp
+++ Algorithm/hdu/ACM_STEP/new_ch3/s1_Recursive/2_bee/2.cpp
@@ -4,8 +4,8 @@
 //#include<vector>
 #include<algorithm>
 
-#define U_DEBUG
-#define L_JUDGE
+//#define U_DEBUG
+//#define L_JUDGE
 
 #ifdef L_JUDGE
 #pragma warning(disable:4996)
@@ -18,6 +18,22 @@ int main(){
 		freopen("in.txt","r",stdin);
 		freopen("out.txt","w",stdout);
 	#endif
+
+	long long f[55];
+	f[0]=0;
+	f[1]=1;
+	f[2]=2;
+	for(int i=3;i<=48;i++){
+		f[i]=f[i-1]+f[i-2];
+	}
+	int N;
+	cin>>N;
+	while(N--){
+		int a,b;
+		cin>>a>>b;
+		int m=b-a;
+		printf("%lld\n",f[m]);
+	}
 
 	#ifdef L_JUDGE
 		fclose(stdin);
