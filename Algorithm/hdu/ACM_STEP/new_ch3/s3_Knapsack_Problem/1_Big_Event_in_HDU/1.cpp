5b02b3cf1206d7a45b23fa036e6c7cdbe9f88429
fangtanchen <chentf0822_account@126.com>
2016-02-05 20:37:35 +0800

    change c3s3 name

--- Algorithm/hdu/ACM_STEP/new_ch3/s3_Knapsack_Problem/1_Big_Event_in_HDU/1.cpp
+++ Algorithm/hdu/ACM_STEP/new_ch3/s3_Knapsack_Problem/1_Big_Event_in_HDU/1.cpp
@@ -13,11 +13,35 @@
 
 using namespace std;
 
+class CFacility{
+public:
+	int value;
+	int num;
+};
+
 int main(){
 	#ifdef L_JUDGE
 		freopen("in.txt","r",stdin);
 		freopen("out.txt","w",stdout);
 	#endif
+	
+	int N;
+	CFacility fa[55];
+	while(EOF!=scanf("%d",&N),N>=0){
+		int sum=0;
+		for(int i=0;i<N;i++){
+			scanf("%d%d",fa[i].value,fa[i].num);
+			sum=sum+fa[i].value*fa[i]*num;
+		}
+		int aver=(sum+1)/2;
+		int dp[55];
+		memset(dp,0,sizeof(dp));
+		for(int i=0;i<N;i++){
+			for(int ni=0;ni<fa[i].num;ni++){
+				
+			}
+		}
+	}
 
 	#ifdef L_JUDGE
 		fclose(stdin);
