922554654ee3d393ee35bd8c56dd98013736a7dd
fangtanchen <chentf0822_account@126.com>
2016-02-21 23:01:03 +0800

    dffdaf

--- Algorithm/hdu/ACM_STEP/ch4_search/s3/6_define_is_unsafe/6.cpp
+++ Algorithm/hdu/ACM_STEP/ch4_search/s3/6_define_is_unsafe/6.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
 #include<cstdio>
 #include<cstring>
-//#include<vector>
+#include<vector>
 #include<algorithm>
+#include<string>
 
 #define U_DEBUG
 #define L_JUDGE
@@ -13,12 +14,31 @@
 
 using namespace std;
 
+
+int str[1000000];
+
 int main(){
 	#ifdef L_JUDGE
 		freopen("in.txt","r",stdin);
 		freopen("out.txt","w",stdout);
 	#endif
+	int T;
+	scanf("%d",&T);
+	while(T--){
+		vector<string>	max_stack;
+		vector<string>  op_stack;
+		vector<string> num_stack;
 
+		scanf("%s",str);
+		int sti=0;
+		int pti=0;
+		int imax=strlen(str);
+		for(pti=0;pti<imax;pti++){
+			if(str[pti]=='M'){
+				
+			}
+		
+		}
 	#ifdef L_JUDGE
 		fclose(stdin);
 		fclose(stdout);
