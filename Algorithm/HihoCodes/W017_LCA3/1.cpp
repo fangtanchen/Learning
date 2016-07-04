210cf6935a534d74e423225013e11dc9b0976fb3
fangtanchen <chentf0822_account@126.com>
2016-07-05 07:59:24 +0800

    LCA online algorithm

--- Algorithm/HihoCodes/W017_LCA3/1.cpp
+++ Algorithm/HihoCodes/W017_LCA3/1.cpp
@@ -18,27 +18,40 @@ using namespace std;
 const int MAXN=100010;
 const int MAXM=100010;
 const int MAXS=200010;
+const int MAXP=400010;
 
 map<string,int> s2i;
 map<int,string> i2s;
-vector<int> G[MAXN];
-int depth[MAXN];
-int in[MAXN];
-int arr[MAXS];
+vector<int> G[MAXS];
+int depth[MAXS];
+int in[MAXS];
+int arr[MAXP];
+int pos[MAXS];
 int ai;
 int n,m;
 int cnt;
 
 
 void DFS(int root){
-    arr[ai]=root;
-    ai++;
+    //arr[ai]=root;
+    //ai++;
     for(int i=G[root].size()-1;i>=0;i--){
-        depth[i]=depth[root]+1;
-        DFS(i);
+        arr[ai++]=root;
+        int kk=G[root][i];
+        depth[kk]=depth[root]+1;
+        DFS(kk);
     }
     arr[ai]=root;
+    pos[root]=ai;
     ai++;
+}
+
+void Init(){
+    s2i.clear();
+    i2s.clear();
+    for(int i=1;i<=MAXS;i++)G[i].clear();
+    memset(depth,0,sizeof(depth));
+    memset(in,0,sizeof(in));
 }
 
 int main(){
@@ -47,6 +60,7 @@ int main(){
 //		freopen("out.txt","w",stdout);
 	#endif
     scanf("%d",&n);
+    Init();
     string name[2];
     cnt=1;
     for(int i=1;i<=n;i++){
@@ -69,11 +83,21 @@ int main(){
             DFS(i);
         }
     }
-    scanf("%d",&M);
-    for(int i=0;i<M;i++){
+    scanf("%d",&m);
+    for(int i=0;i<m;i++){
         cin>>name[0]>>name[1];
-        int tt[2]={0,0};
-
+        int k0=s2i[name[0]],k1=s2i[name[1]];
+        int pos0=pos[k0],pos1=pos[k1];
+        if(pos1<pos0)swap(pos0,pos1);
+        int res=depth[arr[pos0]];
+        int kp=pos0;
+        for(int i=pos0;i<=pos1;i++){
+            if(res>depth[arr[i]]){
+                res=depth[arr[i]];
+                kp=i;
+            }
+        }
+        cout<<i2s[arr[kp]]<<endl;
     }
 
 	#ifdef L_JUDGE
