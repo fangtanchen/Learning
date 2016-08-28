4486512490bdd37b93cbca1ecff3e4ecb262d9c1
fangtanchen <chentf0822_account@126.com>
2016-08-28 21:09:14 +0800

    changtong gongcheng ; ZJU exams

--- Algorithm/hdu/ACM_STEP/ch6/s1/4/4.cpp
+++ Algorithm/hdu/ACM_STEP/ch6/s1/4/4.cpp
@@ -12,12 +12,65 @@
 #endif
 
 using namespace std;
+const int MAXN=105;
+const int INF=0x7f7f7f7f;
+int map[MAXN][MAXN];
+bool vis[MAXN];
+int dis[MAXN];
+int N;
+
+void Init(){
+    memset(vis,false,sizeof(vis));
+    memset(dis,0x7f,sizeof(dis));
+}
+
+int Prim(){
+    vis[1]=true;
+    int ret=0;
+    for(int i=1;i<=N;i++){
+        if(!vis[i]){
+            dis[i]=map[1][i];
+        }
+    }
+    do{
+        int maxi=-1;
+        int maxv=INF;
+        for(int i=1;i<=N;i++){
+            if(!vis[i]) {
+                if(dis[i]<maxv){
+                    maxi=i;
+                    maxv=dis[i];
+                }
+            }
+        }
+        if(maxi==-1)break;
+        vis[maxi]=true;
+        ret+=maxv;
+        for(int i=1;i<=N;i++){
+             if(vis[i])continue;
+             if(map[maxi][i]<dis[i]){
+                 dis[i]=map[maxi][i];
+             }
+        }
+    }while(1);
+    return ret;
+}
 
 int main(){
 	#ifdef L_JUDGE
 		freopen("in.txt","r",stdin);
 		freopen("out.txt","w",stdout);
 	#endif
+    cin.sync_with_stdio(false);
+    while(cin>>N,N){
+        Init();
+        for(int i=N*(N-1)/2;i>=1;i--){
+            int a,b,l;
+            cin>>a>>b>>l;
+            map[a][b]=map[b][a]=l;
+        }
+        cout<<Prim()<<endl;
+    }
 
 	#ifdef L_JUDGE
 		fclose(stdin);
--- Algorithm/hdu/ACM_STEP/ch6/s1/5/5.cpp
+++ Algorithm/hdu/ACM_STEP/ch6/s1/5/5.cpp
@@ -12,12 +12,92 @@
 #endif
 
 using namespace std;
+const int MAXN=105;
+const int INF=0x7f7f7f7f;
+int map[MAXN][MAXN];
+bool vis[MAXN];
+int dist[MAXN];
+int fa[MAXN];
+int N;
+
+void Init(){
+    memset(dist,0x7f,sizeof(dist));
+     memset(vis,false,sizeof(vis));
+     for(int i=1;i<MAXN;i++){
+         fa[i]=i;
+     }
+}
+
+int Find(int x){
+    if(x==fa[x])return x;
+    fa[x]=Find(fa[x]);
+    return fa[x];
+}
+
+int Prim(){
+    int rt=Find(1);
+    vis[rt]=true;
+    for(int i=1;i<=N;i++){
+        int fi=Find(i);
+        if(vis[fi])continue;
+        dist[fi]=map[rt][fi];
+    }
+    int ret=0;
+    do{
+        int maxi=-1;
+        int maxv=INF;
+        for(int i=1;i<=N;i++){
+            int fi=Find(i);
+            if(vis[fi])continue;
+            if(dist[fi]<maxv){
+                 maxi=fi;
+                 maxv=dist[fi];
+            }
+        }
+        if(-1==maxi)break;
+        ret+=maxv;
+        vis[maxi]=true;
+        for(int i=1;i<=N;i++){
+             int fi=Find(i);
+             if(vis[fi])continue;
+             if(map[maxi][fi]<dist[fi]){
+                 dist[fi]=map[maxi][fi];
+             }
+        }
+    }while(1);
+    return ret;
+}
 
 int main(){
 	#ifdef L_JUDGE
 		freopen("in.txt","r",stdin);
 		freopen("out.txt","w",stdout);
 	#endif
+    cin.sync_with_stdio(false);
+    while(cin>>N,N){
+        Init();
+        for(int i=N*(N-1)/2;i>0;i--){
+            int a,b,l,s;
+            cin>>a>>b>>l>>s;
+            map[a][b]=map[b][a]=l;
+            if(s==1){
+                int pa=Find(a),pb=Find(b);
+                if(pa!=pb){
+                    fa[pa]=pb;
+                }
+            }
+        }
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=N;j++){
+                int pa=Find(i),pb=Find(j);
+                if((pa!=pb)&&(map[i][j]<map[pa][pb])){
+                    map[pa][pb]=map[pb][pa]=map[i][j];
+                }
+            }
+        }
+        cout<<Prim()<<endl;
+    }
+
 
 	#ifdef L_JUDGE
 		fclose(stdin);
