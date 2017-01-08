32c94d7dd88309db2e9637189c01704652950b27
fangtanchen <chentf0822_account@126.com>
2017-01-08 23:54:57 +0800

    ff

--- 1_Algorithm/Codeforces/382C_Arithmetic_Progression/1.cpp
+++ 1_Algorithm/Codeforces/382C_Arithmetic_Progression/1.cpp
@@ -29,19 +29,62 @@ int main(){
          scanf("%d",num+ni);
     }
     if(N==1){
-        printf("-1");
+        printf("-1\n");
         return 0;
     }
     sort(num,num+N);
     if(N==2){
         int tmp=num[1]-num[0];
-        if((tmp>1)&&(tmp&0x1==0))printf("3\n");
+        if(tmp==0){
+            printf("1\n%d\n",num[0]);
+            return 0;
+        }
+        if((tmp>1)&&((tmp&0x1)==0))printf("3\n");
         else printf("2\n");
         printf("%d",2*num[0]-num[1]);
-        if((tmp>1)&&(tmp&0x1==0))printf(" %d",);
+        if((tmp>1)&&((tmp&0x1)==0))printf(" %d",(num[0]+num[1])>>1);
         printf(" %d\n",2*num[1]-num[0]);
+        return 0;
     }
-
+    int imax=N-1;
+    for(int i=0;i<imax;i++){
+         d[i]=num[i+1]-num[i];
+    }
+    sort(d,d+imax);
+    int cnt=0;
+    int ud[10];
+    ud[cnt++]=d[0];
+    for(int i=1;i<imax;i++){
+        if(d[i]!=d[i-1]){
+            ud[cnt++]=d[i];
+            if(cnt>2)break;
+        }
+    }
+    int nans=0;
+    int ans[10];
+    if(cnt==1){
+       nans=2;
+       ans[0]=num[0]-ud[0];
+       ans[1]=num[N-1]+ud[0];
+       if(ans[0]==ans[1])nans--;
+    }else if(2==cnt){
+        if(ud[1]==2*ud[0]){
+            for(int i=0;i<imax;i++){
+                if(num[i+1]-num[i]==ud[1]){
+                    ans[nans++]=((num[i+1]+num[i])>>1);
+                    break;
+                }
+            }
+        }
+    }
+    printf("%d\n",nans);
+    if(nans>0){
+        printf("%d",ans[0]);
+        for(int i=1;i<nans;i++){
+            printf(" %d",ans[i]);
+        }
+        printf("\n");
+    }
 
 	#ifdef L_JUDGE
 		fclose(stdin);
