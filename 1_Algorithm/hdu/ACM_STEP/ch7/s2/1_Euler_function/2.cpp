f7ae5c76b1f8364b3855ddf62a8e513b677fd3d8
fangtanchen <chentf0822_account@126.com>
2017-01-12 09:53:44 +0800

    Euler function

---
+++ 1_Algorithm/hdu/ACM_STEP/ch7/s2/1_Euler_function/2.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+//#include<vector>
+#include<algorithm>
+
+#define U_DEBUG
+#define L_JUDGE
+
+#ifdef L_JUDGE
+#pragma warning(disable:4996)
+#endif
+
+using namespace std;
+const int MAXN=3e6+20;
+
+long long int dp[MAXN];
+int primeList[MAXN];
+int cntprime;
+bool bPrime[MAXN];
+
+
+int main(){
+	#ifdef L_JUDGE
+		freopen("in.txt","r",stdin);
+//		freopen("out.txt","w",stdout);
+	#endif
+    memset(dp,0,sizeof(dp));
+    cntprime=0;
+    memset(bPrime,true,sizeof(bPrime));
+    for(int ni=2;ni<MAXN;ni++){
+        if(bPrime[ni]){
+            primeList[cntprime++]=ni;
+            dp[ni]=ni-1;
+        }
+        for(int pi=0;pi<cntprime;pi++){
+            int tmp=ni*primeList[pi];
+            if(tmp>=MAXN)break;
+            bPrime[tmp]=false;
+            if(ni%primeList[pi]==0){
+                dp[tmp]=dp[ni]*primeList[pi];
+                break;
+            }else{
+                dp[tmp]=dp[ni]*dp[primeList[pi]];
+            }
+        }
+    }
+    for(int ni=2;ni<MAXN;ni++){
+        dp[ni]+=dp[ni-1];//bigger than int
+    }
+    int a,b;
+    while(EOF!=scanf("%d%d",&a,&b)){
+         long long int ans=dp[b]-dp[a-1];
+         printf("%lld\n",ans);
+    }
+
+	#ifdef L_JUDGE
+		fclose(stdin);
+		fclose(stdout);
+		system("out.txt");
+	#endif
+
+	return 0;
+}
---
+++ 1_Algorithm/hdu/ACM_STEP/ch7/s2/2/1.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+//#include<vector>
+#include<algorithm>
+#include<cmath>
+
+#define U_DEBUG
+#define L_JUDGE
+
+#ifdef L_JUDGE
+#pragma warning(disable:4996)
+#endif
+
+using namespace std;
+const int MAXN=1e8+10;
+const int MAXM=1e6+10;
+
+int primeList[MAXM];
+int cntPrime;
+bool isPrime[MAXM];
+int dp[MAXM];
+
+void Init(){
+    cntPrime=0;
+    memset(isPrime,true,sizeof(isPrime));
+    memset(dp,0,sizeof(dp));
+    for(int ni=2;ni<MAXM;ni++){
+        if(isPrime[ni]){
+            primeList[cntPrime++]=ni;
+            dp[ni]=ni-1;
+        }
+        for(int pi=0;pi<cntPrime;pi++){
+            int tmp=primeList[pi]*ni;
+            if(tmp>MAXM)break;
+            isPrime[tmp]=false;
+            if(ni%primeList[pi]==0){
+                 dp[tmp]=dp[ni]*(primeList[pi]);
+                 break;
+            }else{
+                dp[tmp]=dp[ni]*(dp[primeList[pi]]);
+            }
+        }
+    }
+}
+
+int main(){
+	#ifdef L_JUDGE
+		freopen("in.txt","r",stdin);
+//		freopen("out.txt","w",stdout);
+	#endif
+    Init();
+    int N;
+    while(scanf("%d",&N),N){
+         if(N<MAXM){
+             printf("%d\n",N-1-dp[N]);
+             continue;
+         };
+         int ans=1;
+         int tmpN=N;
+         for(int pi=0;pi<cntPrime;pi++){
+             int imax=sqrt(N)+1;
+             if(primeList[pi]>=imax)break;
+             if(N%primeList[pi]==0){
+                  N/=primeList[pi];
+                  ans*=dp[primeList[pi]];
+                  while(N%primeList[pi]==0){
+                      N/=primeList[pi];
+                      ans*=primeList[pi];
+                  }
+             }
+         }
+         if(N!=1)ans*=(N-1);
+         printf("%d\n",tmpN-1-ans);
+    }
+
+	#ifdef L_JUDGE
+		fclose(stdin);
+		fclose(stdout);
+		system("out.txt");
+	#endif
+
+	return 0;
+}
