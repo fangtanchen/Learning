e4469182d31e4b1dab8467d23f7de1eeaaf1e595
fangtanchen <chentf0822_account@126.com>
2017-02-03 20:15:37 +0800

    Nim

--- 1_Algorithm/hdu/ACM_STEP/ch8/s2_Nim/1_BraveGame/1.cpp
+++ 1_Algorithm/hdu/ACM_STEP/ch8/s2_Nim/1_BraveGame/1.cpp
@@ -12,12 +12,29 @@
 #endif
 
 using namespace std;
+const int MAXN=1010;
+int sum,dp[MAXN];
+int N,M,C;
 
 int main(){
 	#ifdef L_JUDGE
 		freopen("in.txt","r",stdin);
 //		freopen("out.txt","w",stdout);
 	#endif
+    scanf("%d",&C);
+    while(C--){
+        scanf("%d%d",&N,&M);
+        sum=0;
+        memset(dp,0,sizeof(dp));
+        dp[0]=1;
+        for(int i=M+1;i<=N;i++){
+             if(sum)dp[i]=0;
+             else dp[i]=1;
+             sum=sum+dp[i]-dp[i-M];
+        }
+        if(dp[N]==0)puts("first");
+        else puts("second");
+    }
 
 	#ifdef L_JUDGE
 		fclose(stdin);
