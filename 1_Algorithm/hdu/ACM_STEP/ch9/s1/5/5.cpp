b56fde606e83c2f00b41362f7918aaed807bfe38
fangtanchen <chentf0822_account@126.com>
2017-11-30 15:55:45 +0800

    weighted union set--- Building Block

--- 1_Algorithm/hdu/ACM_STEP/ch9/s1/5/5.cpp
+++ 1_Algorithm/hdu/ACM_STEP/ch9/s1/5/5.cpp
@@ -12,18 +12,62 @@
 #endif
 
 using namespace std;
+const int MAXN=300010;
+const int MAXP=1e6+10;
+
+int fa[MAXN],cnt[MAXN],len[MAXN];
+int N,P;
+
+void Init(){
+    memset(len,0,sizeof(len));
+    for(int i=0;i<MAXN;i++){
+        fa[i]=i;
+        cnt[i]=1;
+    }
+}
+
+int Find(int u){
+    if(u==fa[u])return u;
+    int rt=Find(fa[u]);
+    len[u]=len[u]+len[fa[u]];
+    fa[u]=rt;
+    return rt;
+}
 
 int main(){
-	#ifdef L_JUDGE
-		freopen("in.txt","r",stdin);
-		//freopen("out.txt","w",stdout);
-	#endif
+#ifdef L_JUDGE
+    freopen("in.txt","r",stdin);
+    //freopen("out.txt","w",stdout);
+#endif
+    cin.sync_with_stdio(false);
+    while(cin>>P){
+        Init();
+        while(P--){
+            char op;
+            int x,y;
+            cin>>op;
+            if(op=='M'){
+                cin>>x>>y;
+                int fx=Find(x),fy=Find(y);
+                if(fx!=fy){
+                     fa[fx]=fy;
+                     len[fx]+=cnt[fy];
+                     cnt[fy]+=cnt[fx];
+                }
+            }else{
+                cin>>x;
+                int fx=Find(x);
+                cout<<len[x]<<endl;
+            }
+        }
+    }
 
-	#ifdef L_JUDGE
-		fclose(stdin);
-		fclose(stdout);
-		system("out.txt");
-	#endif
 
-	return 0;
+#ifdef L_JUDGE
+    fclose(stdin);
+    fclose(stdout);
+    system("out.txt");
+#endif
+
+    return 0;
 }
